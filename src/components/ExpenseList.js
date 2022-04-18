63e098f6097e53247f236981913a07f193117ff5
Phil Kieti <philmkieti@hotmail.com>
2022-04-17 23:53:38 -0400

    Added filter expenses bar and functionality

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,27 +1,46 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { AppContext } from '../context/AppContext';
 import ExpenseItem from './ExpenseItem';
 
-export default function ExpenseList() {
+// export default function ExpenseList() {
+const ExpenseList = () => {
     const { expenses } = useContext(AppContext);
-// const ExpenseList = () => {
-    // const expenses2 = [
-    //     { id: 1232341, name: 'Shopping', cost: 50 },
-    //     { id: 1232342, name: 'Holiday', cost: 300 },
-    //     { id: 1232343, name: 'Transportation', cost: 70 },
-    //     { id: 1232344, name: 'Fuel', cost: 40 },
-    //     { id: 1232345, name: 'Child Care', cost: 500 }
-    // ];
+
+    const [filteredExpenses, setFilteredExpenses] = useState(expenses || []);
+    useEffect(() => {
+      setFilteredExpenses(expenses);
+    }, [expenses]);
+    const handleChange = (event) => {
+        const searchResults = expenses.filter((filteredExpense) => 
+            filteredExpense.name.toLowerCase().includes(event.target.value)
+        );
+        setFilteredExpenses(searchResults);
+    };
+    
     return (
-        <ul className='list-group'>
-            {
-                expenses.map((expense) => (
-                // expenses.map(expense => (
-                    <ExpenseItem key={expense.id} id={expense.id} name={expense.name} cost={expense.cost} />
-                ))
-            }
-        </ul>
+        <>
+            <input
+                type='text'
+                className='form-control mb-2 mr-sm-2'
+                placeholder='Type to search...'
+                onChange={handleChange}
+            />
+            <ul className='list-group mt-3 mb-3'>
+                {
+                    filteredExpenses.map((expense) => (
+                        <ExpenseItem key={expense.id} id={expense.id} name={expense.name} cost={expense.cost} />
+                    ))
+                }
+            </ul>
+        </>
+        // <ul className='list-group'>
+        //     {
+        //         expenses.map((expense) => (
+        //             <ExpenseItem key={expense.id} id={expense.id} name={expense.name} cost={expense.cost} />
+        //         ))
+        //     }
+        // </ul>
     );
 }
 
-// export default ExpenseList;
+export default ExpenseList;
